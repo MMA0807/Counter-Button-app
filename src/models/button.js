d394cc2611bcca2f236e1ce892a2ce36415d99ab
MMA0807 <zemistr88@gmail.com>
2020-05-11 03:48:45 +0300

    update

--- src/models/button.js
+++ src/models/button.js
@@ -1,29 +1,32 @@
-import '../styles/button.css'
+import "../styles/button.css";
+
+let count = 0;
+const CLICKS = [" клик ", " клика ", " кликов"];
+
+function declOfNum(number, titles) {
+  const cases = [2, 0, 1, 1, 1, 2];
+  return titles[
+    number % 100 > 4 && number % 100 < 20
+      ? 2
+      : cases[number % 10 < 5 ? number % 10 : 5]
+  ];
+}
 
 class CounterButton {
-
-  count = 0
-  CLICKS = [' клик ', ' клика ', ' кликов']
-
-  declOfNum(number, titles) {
-    const cases = [2, 0, 1, 1, 1, 2];
-    return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
-  }
-  
-  makeCounter() {      
-      this.count++
-      return this.count + this.declOfNum(this.count, this.CLICKS)
+  makeCounter() {
+    count++;
+    return count + declOfNum(count, CLICKS);
   }
 
   render(selector) {
-    selector.insertAdjacentHTML('beforeend', `<button class='btn js-btn'>Click me!</button>`)
-    selector.addEventListener('click', e => {
-      if(e.target.classList.contains('js-btn')) {
-        e.target.innerHTML = this.makeCounter()
-      }
-    })
+    selector.insertAdjacentHTML(
+      "beforeend",
+      `<button class='btn js-btn'>Click me!</button>`
+    );
+    document.querySelector(".js-btn").addEventListener("click", (e) => {
+      e.target.innerHTML = this.makeCounter();
+    });
   }
-  
 }
 
-export default CounterButton
+export default CounterButton;
